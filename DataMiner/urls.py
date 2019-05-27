65e341bed755bb100beeb9db0332241c83856e6c
evandervanwolfswinkel <evandervanwolfswinkel@outlook.com>
2019-05-27 14:52:10 +0200

    Added visualisation files

--- DataMiner/urls.py
+++ DataMiner/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('Diseases/', views.circle_diseases, name='circle_diseases'),
+    path('Compounds/', views.circle_compounds, name='circle_compounds')
 ]
--- DataMiner/views.py
+++ DataMiner/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-
+import VisualisationLogic
 # Create your views here.
 def index(request):
     return render(request, "index.html")
+
+def circle_diseases(request):
+    return render(request, "circle_diseases.html")
+
+def circle_compounds(request):
+    return render(request, "circle_compounds.html")
